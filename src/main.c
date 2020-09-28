ecb1a5363cef2b2f59b839908de0fb889dcaca15
Stefan Eng <eng_steff@outlook.com>
2020-09-28 22:58:52 +0200

    Connect to Unix domain socket for xserver.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
+
 int
 main(void)
 {
-  printf("Hello world\n");
+  int fd_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+  struct sockaddr_un srv_addr = {
+    .sun_family = AF_UNIX,
+    .sun_path = "/tmp/.X11-unix/X0",
+  };
+
+  int result = 0;
+
+  result = connect(
+    fd_socket,
+    (const struct sockaddr *) &srv_addr,
+    sizeof(struct sockaddr_un)
+  );
+
+  if (result == -1) {
+    fprintf(
+      stderr,
+      "Failed to connect to %s with %s.\n",
+      srv_addr.sun_path,
+      strerror(errno)
+    );
+    exit(EXIT_FAILURE);
+  }
 }
